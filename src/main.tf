ae556be7a7454246d017c069c3e42a6e065a75be
Phu Le <phu.le.dev@gmail.com>
2021-09-11 05:05:01 +0000

    UPDATE pipeline, code

--- src/main.tf
+++ src/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "instance" {
   monitoring                  = "true"
   key_name                    = var.key_name
   subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = []
+  vpc_security_group_ids      = var.security_group_ids
 
   tags = {
     Name        = "${var.environment}-demo-instance"
--- src/variables.tf
+++ src/variables.tf
@@ -5,6 +5,7 @@ variable "environment" {
 variable "ubuntu_account_number" {
   type    = string
   default = "099720109477"
+  description = "Owner ID of Ubuntu AWS AMI"
 }
 
 variable "instance_type" {
@@ -15,9 +16,15 @@ variable "instance_type" {
 
 variable "key_name" {
   type        = string
-  description = "EC2 KeyPair Name"
+  description = "EC2 KeyPair Name that EC2 Instance using"
 }
 
 variable "subnet_id" {
   type = string
+  description = "Subnet ID that EC2 Instance running on"
+}
+
+variable "security_group_ids" {
+  type = list(string)
+  description = "List of Security Group IDs attach to the EC2 Instance"
 }
